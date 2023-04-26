df3532faf1adb269ee3aa213dca8507b562f7cf2
Emilia <emiliamoroni12@gmail.com>
2023-04-26 16:40:31 -0300

    modificaciones a modulos

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -6,13 +6,32 @@ class Persona {
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
+   constructor (nombre, apellido, edad, domicilio){
+      this.nombre = nombre,
+      this.apellido = apellido,
+      this.edad = edad,
+      this.domicilio = domicilio,
+      this.detalle = function(){
+      var nuevoObjeto = {
+      nombre : nombre,
+      apellido : apellido,
+      edad : edad,
+      domicilio : domicilio
+   }   
+return nuevoObjeto;
 }
+}
+}
+
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
+   let nuevaInstancia = new Persona (nombre, apellido, edad, domicilio);
+   return nuevaInstancia;
+
 }
 
 function agregarMetodo() {
@@ -20,6 +39,10 @@ function agregarMetodo() {
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
+   const persona = new Persona;
+   persona.datos = function(){
+      return this.nombre + "," + " " + this.edad + "años";
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -5,12 +5,24 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
+   //let primeraLetra = nombre.chartAt(0);
+   //let primeraLetraMayuscula = primeraLetra.toUpperCase();
+   //let nombreCompleto = nombre.slice(1,nombre.length);
+   //let stringNuevo = primeraLetraMayuscula + nombreCompleto;
+  // return stringNuevo;
+  let nombreCortado = nombre.split("");
+  let nuevaMayuscula = nombreCortado[0].toUpperCase();
+  nombreCortado.shift();
+  nombreCortado.unshift(nuevaMayuscula);
+  let resultadoPalabra = nombreCortado.join("");
+  return resultadoPalabra;
 }
 
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   cb();
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -18,14 +30,22 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+   let resultado = cb(num1, num2);
+   return resultado;
 }
 
 function sumarArray(arrayOfNumbers, cb) {
    // Recibes un arreglo de números y un callback.
    // Suma todos los números del arreglo.
    // Este resultado debes pasárselo como argumento al callback recibido.
-   // [NOTA]: no debes reotrnar nada.
+   // [NOTA]: no debes retornar nada.
    // Tu código:
+   let suma = 0;
+   for (let i = 0; i < arrayOfNumbers.length; i++){
+      const numeritos = arrayOfNumbers[i];
+      suma = suma + numeritos;
+   }
+   cb(suma);
 }
 
 function forEach(array, cb) {
@@ -33,6 +53,7 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+   array.forEach(cb);
 }
 
 function map(array, cb) {
@@ -40,12 +61,26 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+   let newArray = [];
+   newArray = array.map(cb);
+
+
+   return newArray;
+  
+
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   let elementos = [];
+   for (let i = 0; i < arrayOfStrings.length; i++){
+      if (arrayOfStrings[i].charAt(0) === "a"){
+         elementos.push(arrayOfStrings[i]);
+      }
+   }
+   return elementos;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
